0b1e1735ca4b07a46e352d5dec066c20c7e8c16b
WilliamHill <william.hill@gmail.com>
2016-02-10 09:47:14 +0000

    AMERCH-30: correct Settings toolbar title

--- app/src/main/java/info/blockchain/merchant/SettingsActivity.java
+++ app/src/main/java/info/blockchain/merchant/SettingsActivity.java
@@ -38,6 +38,7 @@ public class SettingsActivity extends PreferenceActivity	{
 
         LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
         Toolbar toolbar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        toolbar.setTitle(R.string.action_settings);
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         root.addView(toolbar, 0);
