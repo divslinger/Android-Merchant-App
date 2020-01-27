760e722736372a3abe729470c41e921f341b404d
JavaNoobsyBoobsy <xavier.kral@gmail.com>
2020-01-27 16:21:52 +0100

    Simplified Unit Test

---
+++ app/src/test/java/com/bitcoin/merchant/app/BitcoinCashAddressValidationTest.kt
@@ -0,0 +1,36 @@
+package com.bitcoin.merchant.app
+
+import com.bitcoin.merchant.app.util.AddressUtil
+import org.junit.jupiter.api.Test
+
+internal class BitcoinCashAddressValidationTest {
+    @Test
+    fun validateCashAddr() {
+        val address = "bitcoincash:qrjc9yecwkldlhzys3euqz68f78s2wjxw5h6j9rqpq"
+        assert(AddressUtil.isValidCashAddr(address))
+    }
+
+    @Test
+    fun validateCashAddrNoPrefix() {
+        val address = "qrjc9yecwkldlhzys3euqz68f78s2wjxw5h6j9rqpq"
+        assert(AddressUtil.isValidCashAddr(address))
+    }
+
+    @Test
+    fun validateLegacy() {
+        val address = "1MvYASoHjqynMaMnP7SBmenyEWiLsTqoU6"
+        assert(AddressUtil.isValidLegacy(address))
+    }
+
+    @Test
+    fun validateInvalidCashAddr() {
+        val address = "bitcoincash:qrjc9yecwkldlhzys3euqz68f9rqpq"
+        assert(!AddressUtil.isValidCashAddr(address))
+    }
+
+    @Test
+    fun validateInvalidLegacy() {
+        val address = "1MvYASoHjqynMaMnP7SBmenyEWi"
+        assert(!AddressUtil.isValidCashAddr(address))
+    }
+}
