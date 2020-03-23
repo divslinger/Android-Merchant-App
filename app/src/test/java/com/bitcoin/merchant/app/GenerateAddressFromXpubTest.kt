c9153476f785a00b884228d4577a663bfcb40821
pokkst <pokkst@protonmail.com>
2020-03-23 10:44:18 -0500

    Add more unit tests for addresses, API keys, and invoice creation in multiple currencies

--- app/src/test/java/com/bitcoin/merchant/app/GenerateAddressFromXpubTest.kt
+++ app/src/test/java/com/bitcoin/merchant/app/GenerateAddressFromXpubTest.kt
@@ -28,4 +28,21 @@ internal class GenerateAddressFromXpubTest {
         println(legacyAddress)
         assertEquals(legacyAddress, "1HZhTawZTGXaphD2Ut1qhfC2Sij1WKQydE")
     }
+
+    @Test
+    fun verifyDifferentIndexesAreDifferentAddresses() {
+        val xpub = "xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz"
+        val index1 = 0
+        val index2 = 1
+        val accountKey = WalletUtil.createMasterPubKeyFromXPub(xpub)
+        val dk1 = HDKeyDerivation.deriveChildKey(accountKey, ChildNumber(index1, false))
+        val ecKey1 = ECKey.fromPublicOnly(dk1.pubKey)
+        val legacyAddress1 = ecKey1.toAddress(MainNetParams.get()).toBase58()
+        val dk2 = HDKeyDerivation.deriveChildKey(accountKey, ChildNumber(index2, false))
+        val ecKey2 = ECKey.fromPublicOnly(dk2.pubKey)
+        val legacyAddress2 = ecKey2.toAddress(MainNetParams.get()).toBase58()
+        println(legacyAddress1)
+        println(legacyAddress2)
+        assert(legacyAddress1 != legacyAddress2)
+    }
 }
